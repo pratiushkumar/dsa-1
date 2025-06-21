f4725cf4416b389a78781b59991865393589ae92
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-06-21 23:44:38 +0530

    Treeset

---
+++ src/Teest.java
@@ -0,0 +1,15 @@
+import java.util.TreeSet;
+
+public class Teest {
+    public static void main(String[]  args) {
+        TreeSet ts = new TreeSet();
+        ts.add("L");
+        ts.add("P");
+        ts.add("O");
+        ts.add("Y");
+        ts.add("F");
+        ts.add("I");
+
+        System.out.println(ts);
+    }
+}
