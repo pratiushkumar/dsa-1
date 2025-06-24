270835a2f4a683e2cc2e6106c34898429907fac9
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-06-24 23:53:06 +0530

    Non leaf nodes in the tree

---
+++ src/NonLeaf.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node5{
+    int data;
+    Node5 right;
+    Node5 left;
+
+    Node5() {}
+    Node5(int data) {
+        this.data = data;
+        this.left =left;
+        this.right =right;
+    }
+}
+public class NonLeaf {
+    public int NodeNonleaf
+    {
+        Queue<Node5> store = new LinkedList<>();
+        store.offer(root);
+        while(!store.isEmpty()) {
+            Node5 tempo = store.poll();
+            if(tempo.left!= null || )
+        }
+    }
+}
