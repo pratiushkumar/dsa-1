5a75b0c09601331a161bd5c1b75822c1d950d825
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-06-30 23:44:43 +0530

    hashmap

--- src/hashmap.java
+++ src/hashmap.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class hashmap {
@@ -12,14 +13,26 @@ public class hashmap {
         System.out.println("the string is  :" + make);
         for(int i=0;i<make.length();i++) {
             String c = String.valueOf(make.charAt(i));
-            if(store.containsKey(c)) {
-                store.put(String.valueOf(c),store.get(c)+ 1);
+            if (store.containsKey(c)) {
+                store.put(String.valueOf(c), store.get(c) + 1);
+            } else {
+                store.put(String.valueOf(c), 1);
             }
-            else{
-                store.put(String.valueOf(c),1);
-            }
-            if
         }
+        //CVONVERT the set into an array
+            String maxKey = null;
+            int maxValue = 0;
+        Object[] entryArray = store.entrySet().toArray();
+
+        for (int i = 0; i < entryArray.length; i++) {
+            g, Integer> entry = (Map.Entry<String, Integer>) entryArray[i];
+        if (entry.getValue() > maxValue) {
+            maxKey = entry.getKey();
+            maxValue = entry.getValue();
+        }
+    }
+
+        System.out.println("Most frequent character: " + maxKey + " occurred " + maxValue + " times.");
 System.out.println(store);
 
 
