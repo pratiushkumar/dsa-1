a8b031c27d69c642c2e4cc5ded2943f0c6cc6315
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-06-30 23:43:58 +0530

    countnode

--- src/CountNode.java
+++ src/CountNode.java
@@ -1,30 +1,40 @@
-class Node1 {
-    int data;
-    Node1 left;
-    Node1 right;
-
-    Node1() {}
-    Node1(int value) {
-        this.data =value;
-        this.left=null;
-        this.right=null;
-    }
-}
-public class CountNode {
-    public void total(Node1 root,int count) {
-        count++;
-
-        if(root.left != null) {
-            total(root, count);
-        }
-        if(root.right !=  null) {
-            total(root,count);
-        }
-    }
-    public static void main (String[] args) {
-        Node1 top =  new Node1(20);
-
-        CountNode nc  =new CountNode();
-
-    }
-}
+//class Node1 {
+//    int data;
+//    Node1 left;
+//    Node1 right;
+//
+//    Node1() {}
+//    Node1(int value) {
+//        this.data =value;
+//        this.left=null;
+//        this.right=null;
+//    }
+//}
+//public class CountNode {
+//    public void total(Node1 root,int count) {
+//
+//        count++;
+//        System.out.println("this is the count : "+count);
+//
+//        if(root.left != null) {
+//            total(root, count);
+//        }
+//        if(root.right !=  null) {
+//            total(root,count);
+//        }
+//    }
+//    public static void main (String[] args) {
+//        int count;
+//        Node1 top =  new Node1(20);
+//        top.left = new Node1(10);
+//        top.right = new Node1(20);
+//        top.left.left =new Node1(30);
+//        top.left.left.left = new Node1(60) ;
+//        top.right.right = new Node1(50);
+//        CountNode nc  =new CountNode();
+//
+//        CountNode cn = new CountNode();
+//        cn.total(top,count);
+//
+//    }
+//}
