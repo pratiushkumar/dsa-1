9ed249b35c3c025d8149f306eb2a96b4a70d7275
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-07-01 23:54:43 +0530

    Strinconversion

---
+++ src/Stringconversion.java
@@ -0,0 +1,18 @@
+class testing {
+    int boo;
+
+}
+
+
+
+public class Stringconversion {
+
+    public static void main (String[] args) {
+        testing t = new testing();;
+         String     bb = String.valueOf(t.boo=200);
+         System.out.println(bb);
+        
+    }
+
+
+}
