57fa8e9e0c4164957208c40f3c98a328eb697c1f
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-07-31 00:04:06 +0530

    Test

--- src/Teest.java
+++ src/Teest.java
@@ -1,15 +1,36 @@
-import java.util.TreeSet;
-
-public class Teest {
-    public static void main(String[]  args) {
-        TreeSet ts = new TreeSet();
-        ts.add("L");
-        ts.add("P");
-        ts.add("O");
-        ts.add("Y");
-        ts.add("F");
-        ts.add("I");
-
-        System.out.println(ts);
-    }
-}
+//import java.util.Comparator;
+//import java.util.TreeSet;
+//import java.util.*;
+//class Mycomparator implements Comparator
+//{
+//    @Override
+//    public int compare(Object obj1 ,Object obj2) {
+//        //logic coming sooon
+//        System.out.println("for every comparison the compare() is called");
+//        Integer i1 = (Integer)obj1;
+//        Integer i2 = (Integer)obj2;
+//        return i1.compareTo(i2);
+//
+////        if(i1<i2)
+////            return +1;
+////        else if(i1>i2)
+////            return -1;
+////        else
+////            return 0;
+//
+//
+//    }
+//}
+//class Teest {
+//    public static void main(String[]  args) {
+//        TreeSet ts = new TreeSet(new Mycomparator());
+//        ts.add("10");
+//        ts.add("20");
+//        ts.add("8");
+//        ts.add("15");
+//        ts.add("63");
+//        ts.add("7");
+//
+//        System.out.println(ts); //[0,15,20,35,60]
+//    }
+//}
