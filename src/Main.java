235ff26250326a80ef83e98a2932c90917aadebd
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-07-31 00:02:56 +0530

    mainfile

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
+//import java.util.ArrayList;
+//
+//public class Main {
+//
+//    public static void main(String[] args) throws Exception  {
+//        System.out.println("Hello, World!");
+//        Heap<Integer> heap = new Heap<>();
+//        heap.insert(35);
+//        heap.insert(21);
+//        heap.insert(43);
+//        heap.insert(21);
+//        heap.insert(87);
+//
+//        ArrayList list = heap.heapsort();
+//        System.out.println(list);
+//    }
+//}
