ee2b1836005eafc0ac836e194ad47f265a3cc52e
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-08-01 23:55:54 +0530

    input2D

--- src/input2D.java
+++ src/input2D.java
@@ -2,43 +2,43 @@ import java.util.Scanner;
 
 
 
-public class input2D {
-    public static void TwoD(int[][] arr2d, int row, int col) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter the elements: ");
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                arr2d[i][j] = input.nextInt();
-            }
-        }
-
-        // Printing matrix after input
-        System.out.println("2D Array is: ");
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                System.out.print("[ " + arr2d[i][j] + " ] ");
-            }
-            System.out.println();
-        }
-    }
-    public boolean searching(int[][] arr,int target ) {
-        if(arr.length==0 || arr == null)  {
-            return false;
-        }
-        int rows= arr.length;
-        int cols=arr[0].length;
-        int left  = 0;  int right  =  rows * cols -1;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of rows: ");
-        int row = sc.nextInt();
-        System.out.println("Enter the number of columns: ");
-        int col = sc.nextInt();
-
-        int[][] arr = new int[row][col];
-        TwoD(arr, row, col);
-    }
-}
+//public class input2D {
+//    public static void TwoD(int[][] arr2d, int row, int col) {
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("Enter the elements: ");
+//        for (int i = 0; i < row; i++) {
+//            for (int j = 0; j < col; j++) {
+//                arr2d[i][j] = input.nextInt();
+//            }
+//        }
+//
+//        // Printing matrix after input
+//        System.out.println("2D Array is: ");
+//        for (int i = 0; i < row; i++) {
+//            for (int j = 0; j < col; j++) {
+//                System.out.print("[ " + arr2d[i][j] + " ] ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//    public boolean searching(int[][] arr,int target ) {
+//        if(arr.length==0 || arr == null)  {
+//            return false;
+//        }
+//        int rows= arr.length;
+//        int cols=arr[0].length;
+//        int left  = 0;  int right  =  rows * cols -1;
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the number of rows: ");
+//        int row = sc.nextInt();
+//        System.out.println("Enter the number of columns: ");
+//        int col = sc.nextInt();
+//
+//        int[][] arr = new int[row][col];
+//        TwoD(arr, row, col);
+//    }
+//}
