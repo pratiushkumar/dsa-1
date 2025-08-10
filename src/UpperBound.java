8a7ba55213ce666bf0c6d239076df11b65ade087
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-08-10 23:27:03 +0530

    UpperBound

---
+++ src/UpperBound.java
@@ -0,0 +1,11 @@
+public class UpperBound {
+    public static void main(String[] args) {
+        Integer intarr[] = {10,20,30,40,50};
+        BondeGeneric<Integer> intdata  = new BondeGeneric<>(intarr);
+
+//         double avgstr = strData.average();
+
+//this will throw error because bound of our classes are within the numbers
+    }
+
+}
