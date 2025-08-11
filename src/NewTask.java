54208e0b36c88dfb3a2c5c4015eef7dcaf851e88
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-08-11 23:58:37 +0530

    NewTask

--- src/NewTask.java
+++ src/NewTask.java
@@ -1,38 +1,38 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.RecursiveAction;
-import java.util.concurrent.RecursiveTask;
-import java.util.concurrent.SubmissionPublisher;
-
-public class NewTask
-{
-    private long load  =  0;
-    public NewTask(long load) {
-        this.load =  load;
-
-    }
-    protected void compute() {
-        //fork getting divided into smaller parts
-        List<NewTask> subtaskstore =  new ArrayList<>();
-        subtaskstore.addAll(createSubtasks());
-
-        for(RecursiveAction taskstore:  subtaskstore) {
-            taskstore.fork();
-        }
-
-    }
-    //function to create add subtasks
-    private List<NewTask> createSubtasks() {
-        List<NewTask> Subtasks = new ArrayList<>();
-        NewTask subtask1 =  new NewTask(this.load/2);
-        NewTask subtask2 = new NewTask(this.load/2);
-        NewTask subtask3 =  new NewTask(this.load/3);
-    //add tasks
-        Subtasks.add(subtask1);
-        Subtasks.add(subtask2);
-        Subtasks.add(subtask3);
-        return Subtasks;
-    }
-
-}
-
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.concurrent.RecursiveAction;
+//import java.util.concurrent.RecursiveTask;
+//import java.util.concurrent.SubmissionPublisher;
+//
+//public class NewTask
+//{
+//    private long load  =  0;
+//    public NewTask(long load) {
+//        this.load =  load;
+//
+//    }
+//    protected void compute() {
+//        //fork getting divided into smaller parts
+//        List<NewTask> subtaskstore =  new ArrayList<>();
+//        subtaskstore.addAll(createSubtasks());
+//
+//        for(RecursiveAction taskstore:  subtaskstore) {
+//            taskstore.fork();
+//        }
+//
+//    }
+//    //function to create add subtasks
+//    private List<NewTask> createSubtasks() {
+//        List<NewTask> Subtasks = new ArrayList<>();
+//        NewTask subtask1 =  new NewTask(this.load/2);
+//        NewTask subtask2 = new NewTask(this.load/2);
+//        NewTask subtask3 =  new NewTask(this.load/3);
+//    //add tasks
+//        Subtasks.add(subtask1);
+//        Subtasks.add(subtask2);
+//        Subtasks.add(subtask3);
+//        return Subtasks;
+//    }
+//
+//}
+//
