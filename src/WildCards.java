71c4aea9134c04b94478faa5a9084fd51d7dd5a1
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-08-23 23:56:14 +0530

    wildcards

--- src/WildCards.java
+++ src/WildCards.java
@@ -1,16 +1,16 @@
-import java.util.List;
-
-public class WildCards {
-
-    public static void print(List<Number> store) {
-        for(Number n: store) {
-            System.out.println(n);
-        }
-    }
-
-    public static void main(String[] args) {
-        List<Integer> initList = List.of(1,2,3,4);
-        print(initList);//compile time error
-    }
-
-}
+//import java.util.List;
+//
+//public class WildCards {
+//
+//    public static void print(List<Number> store) {
+//        for(Number n: store) {
+//            System.out.println(n);
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        List<Integer> initList = List.of(1,2,3,4);
+//        print(initList);//compile time error
+//    }
+//
+//}
