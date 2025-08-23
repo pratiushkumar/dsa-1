5ddf213fe220462065539087ff86ef4d557ed1fe
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-08-23 23:55:12 +0530

    ReverseLinkedlist

--- src/ReverseLL.java
+++ src/ReverseLL.java
@@ -1,42 +1,42 @@
- class ReverseLL {
-    Node head;
-    class Node {
-        int data;
-        Node next;
-
-        public Node(int data) {
-            this.data = data;
-            this.next = null;
-        }
-
-        }
-        public void adddata(int data) {
-        Node newnode  =  new Node(data);
-        if(head ==null) {
-            head =  newnode;
-            return;
-        }
-        }
-public void reverseIterate() {
-        if(head == null || head.next == null) {
-            return;
-        }
-        Node prevNode = head;
-        Node currNode =head.next;
-        while(currNode != null) {
-            Node nextNode=currNode.next;
-            currNode.next =prevNode;
-
-            //update
-            prevNode =currNode;
-            currNode =nextNode;
-        }
-        head.next = null
-}
-        public static void main(String[] ars ) {
-
-        ReverseLL  obj  = new ReverseLL();
-
-    }
-    }
-
+// class ReverseLL {
+//    Node head;
+//    class Node {
+//        int data;
+//        Node next;
+//
+//        public Node(int data) {
+//            this.data = data;
+//            this.next = null;
+//        }
+//
+//        }
+//        public void adddata(int data) {
+//        Node newnode  =  new Node(data);
+//        if(head ==null) {
+//            head =  newnode;
+//            return;
+//        }
+//        }
+//public void reverseIterate() {
+//        if(head == null || head.next == null) {
+//            return;
+//        }
+//        Node prevNode = head;
+//        Node currNode =head.next;
+//        while(currNode != null) {
+//            Node nextNode=currNode.next;
+//            currNode.next =prevNode;
+//
+//            //update
+//            prevNode =currNode;
+//            currNode =nextNode;
+//        }
+//        head.next = null
+//}
+//        public static void main(String[] ars ) {
+//
+//        ReverseLL  obj  = new ReverseLL();
+//
+//    }
+//    }
+//
