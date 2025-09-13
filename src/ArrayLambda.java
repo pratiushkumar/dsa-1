01f6b21047261782e6c640ec5a0e8de6a0db5722
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-09-13 23:55:20 +0530

    Lambda

---
+++ src/ArrayLambda.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//public class ArrayLambda {
+//    public static void main(String[] args) {}
+//    int[][] arrayStream = { {1, 2, 3}, {4, 5}, {6, 7, 8, 9} };
+//
+//    List<Integer> flatted =  new ArrayList<>();
+//    }
+//}
