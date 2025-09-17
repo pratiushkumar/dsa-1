1a4962c349810fccaab76cf85c38bb1b1f897c87
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-09-17 23:50:00 +0530

    Sample

---
+++ src/Sample.java
@@ -0,0 +1,7 @@
+public class Sample {
+    public static void main(String[] args) {
+        String s = "Hello";
+        s.concat(" World");
+        System.out.println(s.concat("biggy"));
+    }
+}
